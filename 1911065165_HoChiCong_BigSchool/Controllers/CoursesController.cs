007f52ed2f33adc104ec33692ebce12d949c77ec
Công Hồ <cong1772001@gmail.com>
2022-03-21 16:29:03 +0700

    Nộp bài lúc 16h30 ngày 21/03/2022

--- 1911065165_HoChiCong_BigSchool/Controllers/CoursesController.cs
+++ 1911065165_HoChiCong_BigSchool/Controllers/CoursesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _1911065165_HoChiCong_BigSchool.Models;
+using _1911065165_HoChiCong_BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +12,40 @@ namespace _1911065165_HoChiCong_BigSchool.Controllers
     public class CoursesController : Controller
     {
         // GET: Courses
+        private readonly ApplicationDbContext _dbcontext;
+        public CoursesController()
+        {
+            _dbcontext = new ApplicationDbContext();
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbcontext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };  
+            _dbcontext.Courses.Add(course);
+            _dbcontext.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbcontext.Categories.ToList()
+            };
+            return View(viewModel);
         }
-        //comit lan1
+
     }
 }
